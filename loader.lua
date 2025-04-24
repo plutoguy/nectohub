829a227989cb9f39e6d67690f1f88a8ca293c28f
plutoguy <123932868+plutoguy@users.noreply.github.com>
2025-04-24 21:50:08 +0100

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,11 @@
+local games = {
+  {"Pets Go",18901165922,"petsgo.lua"},
+  {"Obby But You Lose FPS",119208928703288,"obbybutyoulosefps.lua"},
+  {"Bubble Gum Simulator Infinity",85896571713843,"bubblegumsimulatorinfinity.lua"}
+}
+
+for i,gameinfo in ipairs(games) do
+    if game.PlaceId == gameinfo[2] then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/plutoguy/nectohub/refs/heads/main/"..gameinfo[3]))()
+    end
+end
