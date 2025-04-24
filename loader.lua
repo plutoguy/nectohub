7bec8d8cbe7a6ac1cdc74ecaf7dd3c0078c2c5eb
plutoguy <123932868+plutoguy@users.noreply.github.com>
2025-04-24 22:01:02 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,8 @@
 local games = {
   {"Pets Go",18901165922,"petsgo.lua"},
   {"Obby But You Lose FPS",119208928703288,"obbybutyoulosefps.lua"},
-  {"Bubble Gum Simulator Infinity",85896571713843,"bubblegumsimulatorinfinity.lua"}
+  {"Bubble Gum Simulator Infinity",85896571713843,"bubblegumsimulatorinfinity.lua"},
+  {"Gunfight Arena",85896571713843,"gunfightarena.lua"}
 }
 
 for i,gameinfo in ipairs(games) do
